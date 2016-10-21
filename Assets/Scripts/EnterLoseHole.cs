2c6e30d2b592e215384f6d9a29ebf22eb3c3b6e4
Mferet11 <mferet11@gmail.com>
2016-10-21 14:42:56 -0400

    Added two Holes including winning and losing hole. Added script so when player enters losing hole game pauses. When player enters winning hole the floor drops out basically. Added new instances of level. Also changed GameObject organization. Need to add UI's to restart game, highscore counter, burndown chart.

---
+++ Assets/Scripts/EnterLoseHole.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterLoseHole : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Player encounters lose hole");
+		//pauses game
+		Time.timeScale = 0.0f;
+
+		//Display UI
+
+		//PlayerController.Player.dead
+		//Destroy(other.gameObject);
+		//GameManager.RestartLevelOne
+	}
+}
---
+++ Assets/Scripts/EnterWinHole.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnterWinHole : MonoBehaviour {
+
+	void OnTriggerEnter(Collider other) {
+		Debug.Log ("Player encounters win hole");
+		this.GetComponentInParent<MeshCollider> ().convex = true;
+		this.GetComponentInParent<MeshCollider> ().isTrigger = true;
+	}
+
+}
